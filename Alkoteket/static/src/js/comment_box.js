1cdf9524c4bd0489599e26cf65cda99e5f4f4846
Royne Reinholdsson <royne@outlook.com>
2023-02-22 13:49:55 +0000

    Added a comment box without styling. Retrieves data from drinkcontroller

--- Alkoteket/static/src/js/comment_box.js
+++ Alkoteket/static/src/js/comment_box.js
@@ -1,7 +1,9 @@
 // Check if current URL path is '/drinkview'
 if (window.location.pathname === "/drinkview") {
+  console.log("----------------------------HEJ HEJ HEJ----------------");
   // Define a module named 'show_drinks'
-  odoo.define("show_drinks", function (require) {
+  odoo.define("comment_box", function (require) {
+    console.log("----------------------------DÅ DÅ DÅ DÅ----------------");
     // Import 'web.ajax' module
     var ajax = require("web.ajax");
 
@@ -10,23 +12,26 @@ if (window.location.pathname === "/drinkview") {
 
     // Call an RPC method to retrieve drink data based on the 'id' parameter
     ajax.rpc("/alkoteket/drink/" + id).then(function (data) {
+      console.log(
+        "---------------------------- !!!!!!!!!!!!!!!!!!!!!!!!!! ----------------"
+      );
+
+      console.log(data);
+
       // Parse the data returned from the server and store it in an array named 'drink'
       var drink = [JSON.parse(data)];
+      console.log(drink);
 
-      // Define a variable to store the HTML template for displaying ingredients
-      var reviewTemplate;
+      // Define a variable to store the HTML template for displaying reviews
+      var reviewTemplate = "";
 
       // Loop through each drink in the 'drink' array
       drink.forEach(function (drink) {
-        // Loop through each ingredient in the 'ingredients' array of the current drink
-        for (let index = 0; index < drink.ingredients.length; index++) {
-          const element = drink.ingredients[index];
-
-          // Add a new paragraph to the 'reviewTemplate' variable for the current ingredient
-          reviewTemplate += `<p>${
-            element.name + " " + element.qty + " cl"
-          }</p>`;
-        }
+        // Loop through each review in the 'reviews' array of the current drink
+        drink.reviews.forEach(function (review) {
+          // Add a new paragraph to the 'reviewTemplate' variable for the current review
+          reviewTemplate += `<p>Score: ${review.score}<br>Review: ${review.review}<br>Created By: ${review.reviewer_name}</p>`;
+        });
 
         // Insert the 'reviewTemplate' HTML into the 'comment-box' element on the page
         document
